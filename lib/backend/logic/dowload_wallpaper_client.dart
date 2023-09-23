2e7c2711341fa98d4479632422f2da1d7fe405b7
Akisanya Temiloluwa <tolutech2004@gmail.com>
2023-09-23 21:22:41 +0100

    Download

--- lib/backend/logic/dowload_wallpaper_client.dart
+++ lib/backend/logic/dowload_wallpaper_client.dart
@@ -12,17 +12,38 @@ class DownloadWallpaperClient {
     final externalStorage = await getApplicationDocumentsDirectory();
     try {
       // ignore: unused_local_variable
-      final downloadTask = FlutterDownloader.enqueue(
+      await FlutterDownloader.enqueue(
         url: wallpaperUrl,
         savedDir: externalStorage.path,
         fileName: wallpaperName,
         showNotification: true,
         openFileFromNotification: false,
       );
+
       return DownloadResult.success;
     } catch (e) {
       error = e.toString();
       return DownloadResult.failure;
     }
   }
+
+  // THIS IS TO SET THE WALLPAPER TOTALLY
+
+  // Future<String?> getDownloadedFilePath(String taskId) async {
+  //   try {
+  //     List<DownloadTask> tasks = await FlutterDownloader.loadTasks();
+  //     DownloadTask? task = tasks.firstWhere(
+  //       (task) =>
+  //           task.taskId == taskId && task.status == DownloadTaskStatus.complete,
+  //       orElse: () => null,
+  //     );
+
+  //     if (task != null) {
+  //       return task.savedDir + '/' + task.filename;
+  //     }
+  //   } catch (e) {
+  //     print('Error getting downloaded file path: $e');
+  //   }
+  //   return null;
+  // }
 }
